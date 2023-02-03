5663d0d680e3dd703a415151391fc09f85fd02b5
Dario Balmaceda <dario26081993@uotlook.com.ar>
2023-02-03 14:44:13 -0300

    Control get de los id erroneos o que no figuren en la db

--- api/src/Controllers/Get/getEventById.js
+++ api/src/Controllers/Get/getEventById.js
@@ -4,8 +4,9 @@ const router = Router();
 
 router.get("/:id", async (req, res, next)=>{
     const {id} = req.params
+    const eventId = await Event.findByPk(id)
     try {
-        if(id){
+        if(eventId != null){
             const eventById = await Event.findOne({
                 where:{
                     id: id
@@ -43,7 +44,7 @@ router.get("/:id", async (req, res, next)=>{
             res.status(200).send(eventId)
         }
         else{
-            res.status(404).send("Id Incorrecto")
+            res.status(404).send("El Evento que quiere ver no existe")
         }
     } catch (error) {
         res.status(500)
--- api/src/Controllers/Get/getUserById.js
+++ api/src/Controllers/Get/getUserById.js
@@ -4,17 +4,18 @@ const router = Router();
 
 router.get("/:id", async (req, res, next)=>{
     const {id} = req.params
+    const userId = await User.findByPk(id)
+    console.log(userId)
     try {
-        if(id){
+        if(userId != null){
             const userById = await User.findOne({
                 where:{
                     id: id
                 }
             })
-
+            
             res.status(200).send(userById)
-        }
-        else if(id != User.findByPk(id)) res.status(404).send("El id es incorrecto")
+        }else res.status(404).send("El Usuario que quiere ver NO existe")
     } catch (error) {
         res.status(404)
         next(error)
