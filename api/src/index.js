c2868bb3a55a77e8f14c3c0d79ec71de68533355
BrayanMen <brayanjmr880@gmail.com>
2023-02-04 16:49:15 -0300

    Probando subir la base de datos

--- api/src/index.js
+++ api/src/index.js
@@ -20,7 +20,8 @@
 
 const server = require("./app.js")
 const {conn} = require("./db.js")
-const DB_PORT = process.env.PORT || 4000;
+const DB_PORT = process.env.PGPORT || 3001;
+
 
 
 conn.sync({force: false}).then(()=>{
