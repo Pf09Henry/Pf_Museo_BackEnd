646edb53682d26388b613ee11c5c3ce5db8422e0
DelgadoAlan <100224015+DelgadoAlan@users.noreply.github.com>
2023-02-08 20:07:31 -0300

    from/post y put cloudinary ok

--- api/src/Controllers/Post/PostEvent.js
+++ api/src/Controllers/Post/PostEvent.js
@@ -1,36 +1,54 @@
-const {Event, Guide, Category} = require("../../db")
-const { Router } = require('express');
-const {uploadImage} = require("../../Utils/Cloudinary");
+const { Event, Guide, Category } = require("../../db");
+const { Router } = require("express");
+const { uploadImage } = require("../../Utils/Cloudinary");
 const router = Router();
 
+router.post("/post", async (req, res, next) => {
+  const {
+    name,
+    startDay,
+    endDay,
+    price,
+    img,
+    information,
+    guide,
+    category,
+    status,
+  } = req.body;
 
-router.post("/post", async (req, res, next)=>{
-
-    const {name, startDay, endDay, price, img, information, guide, category, status} = req.body
-    
-    try {
-     if(req.files.img){
-             const result = await uploadImage(req.files.img.tempFilePath)
-             const img = result
-             const newEvent = await Event.create({name, startDay, endDay, price, img, information, status})
-            let guideDb = await Guide.findAll({
-                where:{
-                    name: guide
-                }
-            })
-            let catDb = await Category.findAll({
-                where:{
-                    name: category
-                }
-            })
-            newEvent.addCategory(catDb)
-            newEvent.addGuide(guideDb)
-            res.status(200).send(newEvent)
-        }
-    } catch (error) {
-        next(error)
-        res.status(500)
+  try {
+    if (req.files.img) {
+      const result = await uploadImage(req.files.img.tempFilePath);
+      const img = result;
+      const newEvent = await Event.create({
+        name,
+        startDay,
+        endDay,
+        price,
+        img,
+        information,
+        guide,
+        category,
+        status,
+      });
+      let guideDb = await Guide.findAll({
+        where: {
+          name: guide,
+        },
+      });
+      let catDb = await Category.findAll({
+        where: {
+          name: category,
+        },
+      });
+      newEvent.addCategory(catDb);
+      newEvent.addGuide(guideDb);
+      res.status(200).send(newEvent);
     }
-})
+  } catch (error) {
+    next(error);
+    res.status(500);
+  }
+});
 
 module.exports = router;
