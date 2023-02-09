65931fc804a74fb2271d39cd9082a78f0298042f
David Gabriel Barboza <davidgabrielb@hotmail.com>
2023-02-09 16:24:47 -0300

    actualizaciones

---
+++ api/src/Controllers/Get/getTicketById.js
@@ -0,0 +1,49 @@
+const { Router } = require('express');
+const { Ticket, Event, User } = require("../../db")
+const router = Router();
+
+router.get('/:id', async (req, res) => {
+
+
+    try {
+        const { id } = req.params;
+        const ticketId = await Ticket.findByPk(id, {
+            include: [
+                {
+                    model: Event,
+                    attributes: ['name', "img"]
+                },
+                {
+                    model: User,
+                    attributes: ["name"]
+                }
+            ],
+            attributes: {
+                exclude: ['userId', 'eventId']
+            },
+
+            where: { status: true }
+        });
+        // const ticketId = await Ticket.findOne({
+        //     where: {
+        //         id: id,
+        //         status: true
+        //     },
+        //     include: [
+        //         {
+        //             model: Event,
+        //             attributes: ['name', "img"]
+        //         },
+        //         {
+        //             model: User,
+        //             attributes: ["name"]
+        //         }
+        //     ]
+        // })
+        res.status(200).json(ticketId);
+    } catch (error) {
+        res.status(404).json(error);
+    }
+});
+
+module.exports = router;
