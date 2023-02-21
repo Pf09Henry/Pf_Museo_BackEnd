7be56045c4348e991a0504e582b31b90c0873123
eldeividrig <davidgabrielb@hotmail.com>
2023-02-21 20:57:50 -0300

    correo

--- api/src/Controllers/Post/PostUser.js
+++ api/src/Controllers/Post/PostUser.js
@@ -2,29 +2,34 @@ const { User, Role } = require("../../db")
 const { Router } = require('express');
 const router = Router();
 const { uploadImage } = require("../../Utils/Cloudinary");
-const  sendMail  = require("../../Utils/sendmail");
+const sendMail = require('../../Utils/sendmail');
 
 
 
 router.post("/post", async (req, res, next) => {
-    const { name, email,image, password, phone } = req.body
+    const { name, email, image, password, phone } = req.body
     //const emailSubs = await Subscription.findAll({
     //  where: {email: email}
     //})
 
     try {
-            const status = true
-            let role = await Role.findOne({
-                where: {
-                    name: 'user',
-                },
-            });            
-            let roleId = role.dataValues.id
-            const result = await uploadImage(req.body.image)
-            const image = result
-            const newUser = await User.create({ name, image, email, password, phone, status, roleId })
-            // sendMail(email, "welcome", "Mail trolo")
-            res.status(200).send(newUser)
+        const status = true
+        let role = await Role.findOne({
+            where: {
+                name: 'user',
+            },
+        });
+        let roleId = role.dataValues.id
+        const result = await uploadImage(req.body.image)
+        const image = result
+        const newUser = await User.create({ name, image, email, password, phone, status, roleId })
+        let datos = {
+            mail: email,
+            subject: 'Cuenta Registrada',
+            message: 'Te has registrado exitosamente en el Museo'
+        }
+        sendMail(datos)
+        res.status(200).send(newUser)
     } catch (error) {
         next(error)
         res.status(500)
--- api/src/Utils/PostEmail.js
+++ api/src/Utils/PostEmail.js
@@ -5,6 +5,7 @@ const router = Router();
 router.post("/send_email", (req, res, next)=>{
     try {
         sendMail(req.body)
+        console.log(req.body);
         res.status(200).send(req.body.mail)
     } catch (error) {
         next(error)
