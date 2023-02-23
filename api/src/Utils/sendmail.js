d3e307042b7df0f8b9bdcc8564319d35e30e753b
eldeividrig <davidgabrielb@hotmail.com>
2023-02-23 17:28:57 -0300

    sendMail

--- api/src/Utils/sendmail.js
+++ api/src/Utils/sendmail.js
@@ -1,37 +1,71 @@
-const nodemailer= require ("nodemailer")
+const nodemailer = require("nodemailer")
 
-function sendMail({mail, subject, message}){
-  console.log(mail, subject, message);
-    const config = {
-      host: "smtp.gmail.com",
-      port: 587,
-      auth: {
-        user: "pf09henry@gmail.com",
-        pass: "yvsbutzpqcuaslwe",
-      },
-    };
-    const mail_config = {
+function sendMail({ mail, subject, message, ticket }) {
+  // console.log(mail, subject, message, ticket);
+  const config = {
+    host: "smtp.gmail.com",
+    port: 587,
+    auth: {
+      user: "pf09henry@gmail.com",
+      pass: "yvsbutzpqcuaslwe",
+    },
+  };
+  let mail_config = {}
+  if (ticket != undefined) {
+    console.log(typeof ticket);
+    mail_config = {
       from: "pf09henry@gmail.com",
       to: `${mail}`,
       subject: `${subject}`,
       html: `<div
-            style="
-              background: linear-gradient(to right, #1d2671, #ff4e5b);
-              height: 100vh;
-              justify-content: center;
-              width: 500px;
-              height: 500px;
-              border-radius: 30px;
-              padding: 20px;
-              font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-            "
-          >
-            
-            <h1 style="font-size: 30px; color: rgb(214, 214, 241); text-align: center">
-              ${message}
-            </h1>
-          </div>`,
-  } 
+              style="
+                background: linear-gradient(to right, #1d2671, #ff4e5b);
+                height: 100vh;
+                justify-content: center;
+                width: 500px;
+                height: 500px;
+                border-radius: 30px;
+                padding: 20px;
+                font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+              "
+            >
+              
+              <h1 style="font-size: 30px; color: rgb(214, 214, 241); text-align: center">
+                ${message}
+              </h1>
+              <h1 style="font-size: 30px; color: rgb(214, 214, 241); text-align: center">
+              Cantidad: ${ticket.amount}
+              </h1>
+              <h1 style="font-size: 30px; color: rgb(214, 214, 241); text-align: center">
+                Total: ${ticket.totalOfPurchase}
+              </h1>
+            </div>`,
+    }
+  }else{
+    mail_config = {
+      from: "pf09henry@gmail.com",
+      to: `${mail}`,
+      subject: `${subject}`,
+      html: `<div
+              style="
+                background: linear-gradient(to right, #1d2671, #ff4e5b);
+                height: 100vh;
+                justify-content: center;
+                width: 500px;
+                height: 500px;
+                border-radius: 30px;
+                padding: 20px;
+                font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+              "
+            >
+              
+              <h1 style="font-size: 30px; color: rgb(214, 214, 241); text-align: center">
+                ${message}
+              </h1>              
+            </div>`,
+    }
+  }
+  
   const transport = nodemailer.createTransport(config);
   transport.sendMail(mail_config)
 }
